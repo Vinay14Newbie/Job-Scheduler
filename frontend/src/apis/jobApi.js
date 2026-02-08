172819526070aa13054b61d535cd5cb5139c1a16
Vinay14Newbie <wjust1984@gmail.com>
2026-02-08 18:11:15 +0530

    Added deployed server to serve apis

--- frontend/src/apis/jobApi.js
+++ frontend/src/apis/jobApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://job-scheduler-o36z.onrender.com/api",
 });
 
 export const fetchJobs = (filters = {}) =>
