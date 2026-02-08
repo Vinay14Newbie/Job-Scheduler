600dc31fe1df2cd60cdfaaab84dec7dfb43017d8
Vinay14Newbie <wjust1984@gmail.com>
2026-02-08 11:56:52 +0530

    api layer for frontend added

---
+++ frontend/src/apis/jobApi.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const API = axios.create({
+  baseURL: "http://localhost:3000/api",
+});
+
+export const fetchJobs = (filters = {}) =>
+  API.get("/jobs", { params: filters });
+
+export const fetchJobById = (id) => API.get(`/jobs/${id}`);
+
+export const runJob = (id) => API.post(`/run-job/${id}`);
+
+export const createJob = (data) => API.post("/jobs", data);
+
+export const deleteJob = (id) => API.delete(`/jobs/${id}`);
